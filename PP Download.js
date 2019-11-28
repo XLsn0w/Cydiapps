eb599eb94f83b2dde1bcfd65a618a39ad55aadc7
XLsn0w <xlsn0wios@gmail.com>
2019-11-28 14:13:54 +0800

    add

--- PP Download.js
+++
@@ -1,6 +0,0 @@
-javascript:(function(){
-	html = document.body.innerHTML;
-	url = html.match(/appdownurl=".*?"/);
-	url = url.toString().replace(/\"|appdownurl=/g, "");
-	window.open(atob(url),'_self');
-})()
